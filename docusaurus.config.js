42c400d8d5217eb449280f295553dcd8a4fca6f3
Aman Mavai <amanmavai@gmail.com>
2022-11-19 11:47:33 +0530

    add blog title and description

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,6 +40,9 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
+          blogTitle: 'Docusaurus blog!',
+          blogDescription: 'A Docusaurus powered blog!',
+          postsPerPage: 'ALL',
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
