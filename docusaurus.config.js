5e1baa83bc5f118bd840244d614659a435ee08f0
Aman Mavai <amanmavai@gmail.com>
2022-11-20 10:27:32 +0530

    update Blogs title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "Blogs",
+        title: "Blog",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
